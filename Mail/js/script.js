cd726beb52f0611c92b2beeddd76fbcbecb11892
JacopoLombardi <lombajacopo@gmail.com>
2024-02-23 19:24:43 +0100

    [add] Finished Extra Mail

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -4,8 +4,8 @@
 
 
 // EXTRA -- collego le variabili Js con Tag HTML
-const pMailList = document.querySelector('p');
-let mailUser = document.querySelector('.input_mail');
+const pMailList = document.querySelector('.mail_permesse');
+const mailUser = document.querySelector('.input_mail');
 const check = document.querySelector('.input_btn');
 const pOutputText = document.querySelector('.output_text');
 
@@ -15,30 +15,28 @@ const mailList = ['mail_1', 'mail_2', 'mail_3', 'mail_4', 'mail_5', 'mail_6'];
 // EXTRA -- stampo su HTML tutte le mail consentite
 pMailList.innerHTML += mailList.join(' -- ');
 
-// variabile access per regolare i messaggi in uscita
-let access = 'denied';
-
 
 // EXTRA -- al click del Check esegui il controllo della mail
 check.addEventListener('click', function(){
 
-   // assegno a mailUser il valore letto dell'input
-   mailUser = mailUser.value;
+   // variabile che serve da rilevatore per il ciclo For, perchè quando veniva inserita una mail giusta,
+   // in output stampava sia il messaggio della mail giusta, sia quello della mail sbagliata dato che 
+   // il For continuava, e una volta in fondo, anche la condizione di riga 36 si avverava.
+   let x = '';
 
    // creo un For per controllare se la mail dell'utente è presente nella lista delle mail consentite
-   for (let i=0; i<mailList.length; i++) {
+   for (let i=0; i<mailList.length; i++){
 
-      if (mailUser == mailList[i]){
-         access = 'allowed';
+      if (mailUser.value === mailList[i]){        
          console.log('La tua Mail può accedere');
          pOutputText.innerHTML = `La tua Mail può accedere`    // EXTRA
+         x = 'mail controllata';
       }
-   }
-
-   // se il valore della variabile Access non viene modificato nel For con Allowed avremmo il seguente messaggio
-   if (access === 'denied'){
-      console.log('la tua Mail non ha i permessi per accedere!!'); 
-      pOutputText.innerHTML = `la tua Mail non ha i permessi per accedere!!`    // EXTRA
+      // se il valore della variabile Access non viene modificato nel For con Allowed avremmo il seguente messaggio
+      else if (i === (mailList.length - 1) && (x != 'mail controllata')){
+         console.log('la tua Mail non ha i permessi per accedere!!'); 
+         pOutputText.innerHTML = `la tua Mail non ha i permessi per accedere!!`    // EXTRA
+      }  
    }
 
 })
